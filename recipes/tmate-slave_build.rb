c3b034d858cd1cd29ca00f1deb3f8d805f61527d
JJ Asghar <jj.asghar@peopleadmin.com>
2013-10-24 17:46:26 -0500

    fixed part of the tmate-slave build

--- recipes/tmate-slave_build.rb
+++ recipes/tmate-slave_build.rb
@@ -14,6 +14,7 @@ script "compile_tmate-slave" do
         STATUS=0
         ./create_keys.sh | tee /root/tmate-slave-footprints.txt || STATUS=1
         sed -i s%#define TMATE_DOMAIN "tmate.io"%#define TMATE_DOMAIN "node[:tmate-slave][:domain]"% tmate.h || STATUS=1
+        cd /tmp/tmate-slave
         ./autgen.sh || STATUS=1
         ./configure || STATUS=1
         make || STATUS=1
